08e77073560eef2d679b86df746e995219fce0a9
raza <razamehr2@gmail.com>
2024-11-14 22:23:38 +0400

    Simple sums

---
+++ Exercise 02/simple sum.py
@@ -0,0 +1,11 @@
+# Initialize variables with integer values
+n1= 8
+n2 = 10
+n3 = 34
+n4= 56
+
+# Calculate the sum of the four numbers
+sumofnumbers =n1 + n2 + n3 + n4
+
+# Display the result
+print("The sum of", n1, n2, n3, "and",n4, "is:",sumofnumbers)
