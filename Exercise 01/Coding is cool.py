cf16f519e37629c5c2446667595a382fb04f6cb8
raza <razamehr2@gmail.com>
2024-11-14 21:42:26 +0400

    Coding is cool

--- Exercise 01/Coding is cool.py
+++
@@ -1,7 +0,0 @@
-# define the words to variables
-a = "Coding"
-b = "is"
-c = "Cool"
-
-# adopbt the variables with spaces and print the final message
-print(f"{a} {b} {c}")  # Output: "Coding is Cool"
