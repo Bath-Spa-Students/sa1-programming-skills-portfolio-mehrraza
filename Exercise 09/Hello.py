1137509f7213bc34aca84176d4ad4fa69132fadb
raza <razamehr2@gmail.com>
2024-11-15 00:16:55 +0400

    Hello.py

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,9 @@
+def hello():
+    print("Hello")  # # Outputs "Hello" to the screen
+
+def main():
+    hello()  # # Displays "Hello" on the console
+
+# The following line checks if this script is being run directly
+if __name__ == "__main__":
+    main()  # This code calls the main() function if the script is run directly
